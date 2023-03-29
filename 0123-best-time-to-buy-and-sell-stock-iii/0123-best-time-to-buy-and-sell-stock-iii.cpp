7ae0171e0cfd88f557db7fba8ce563f5701921e1
Arya_Shanish <70702833+Aryashanish@users.noreply.github.com>
2023-03-29 18:17:29 +0530

    Time: 751 ms (35.97%), Space: 218 MB (11.36%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int healper(vector<int>& p , int i , int buy ,int cap , vector<vector<vector<int>>>& dp){
+        if(i == p.size() || cap == 0)
+            return 0;
+        
+        if(dp[i][buy][cap] != -1)
+            return dp[i][buy][cap];
+        
+        int profit = 0;
+        if(buy){
+            profit = max(-p[i] + healper(p,i+1,0,cap,dp) , 
+                            0  + healper(p,i+1,1,cap,dp));
+        }else
+            profit = max(p[i] + healper(p,i+1,1,cap-1,dp) , 
+                            0  + healper(p,i+1,0,cap,dp));
+        
+        return dp[i][buy][cap] = profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n , vector<vector<int>>(2, vector<int>(3,-1)));
+        return healper(prices,0,1,2,dp);
+    }
+};
