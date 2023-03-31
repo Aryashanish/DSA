698650b3bd069b80908a06ab151d61ed1e38abd0
Arya_Shanish <70702833+Aryashanish@users.noreply.github.com>
2023-03-31 16:47:42 +0530

    Time: 1603 ms (5.60%), Space: 292.2 MB (18.14%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int healper(vector<int>& nums , int ind , int p_ind , vector<vector<int>>& dp){
+        if(ind == nums.size())
+            return 0;
+        
+        if(dp[ind][p_ind+1] != -1)
+            return dp[ind][p_ind+1];
+        
+        int len = 0;
+        len = 0 + healper(nums,ind+1,p_ind,dp);
+        if(p_ind == -1 || nums[ind] > nums[p_ind])
+            len = max(len , 1+healper(nums,ind+1,ind,dp));
+        
+        return dp[ind][p_ind+1] = len;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<vector<int>> dp(n , vector<int>(n+1 , -1));
+        return healper(nums,0,-1,dp);
+    }
+};
