53772604860036eabb4240ec1534ffa6e6466f90
Arya_Shanish <70702833+Aryashanish@users.noreply.github.com>
2023-04-03 17:23:09 +0530

    Time: 66 ms (49.78%), Space: 31 MB (12.82%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
+    int healper(vector<int>& w , int n){
+        int daysNeeded = 1, currWeight = 0;
+        for (int weight : w) {
+            if (currWeight + weight > n) {
+                daysNeeded++;
+                currWeight = 0;
+            }
+            currWeight = currWeight + weight;
+        }
+        return daysNeeded;
+    }
     int shipWithinDays(vector<int>& weights, int days) {
-        int maxWeight = -1, totalWeight = 0;
-        for (int weight : weights) {
-            maxWeight = max(maxWeight, weight);
-            totalWeight = totalWeight + weight;
+        int s = 0;
+        int maxi = weights[0];
+        for(int i=0; i<weights.size(); i++){
+            s += weights[i];
+            maxi = max(maxi , weights[i]);
         }
-        //here weight and total weight work as left and right pointer of bunary search
-        while (maxWeight < totalWeight) {
-            int midWeight = maxWeight + (totalWeight - maxWeight) / 2;
-            int daysNeeded = 1, currWeight = 0;
-            for (int weight : weights) {
-                if (currWeight + weight > midWeight) {
-                    daysNeeded++;
-                    currWeight = 0;
-                }
-                currWeight = currWeight + weight;
-            }
-            if (daysNeeded > days) {
-                maxWeight = midWeight + 1;
-            } else {
-                totalWeight = midWeight;
-            }
+        
+        int i = maxi  , j = s;
+        int mid;
+
+        while(i<=j){
+            mid = j+(i-j)/2;
+            int d = healper(weights,mid);
+
+            if(d > days)
+                i = mid+1;
+            else
+                j = mid-1;
         }
-        return maxWeight;
+        return i;
     }
 };
