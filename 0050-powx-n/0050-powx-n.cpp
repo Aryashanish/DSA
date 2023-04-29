4f88cd6d22750d06a1c8f70f950f88f4abd9b389
Arya_Shanish <70702833+Aryashanish@users.noreply.github.com>
2023-04-29 10:08:48 +0530

    Time: 5 ms (11.35%), Space: 6 MB (37.78%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    double healper(double x , int n){
+    double myPow(double x, int n) {
         if(n == 0)
             return 1;
         
-        return (x * myPow(x,n-1));
-    }
-    double myPow(double x, int n) {
-        if (n == INT_MAX) return (x == 1) ? 1 : (x == -1) ? -1 : 0;
-        if (n == INT_MIN) return (x == 1 || x == -1) ? 1 : 0;
-        
         if(n < 0){
-            double temp = healper(x,-n);
-            return (1.0/temp);
+            n = abs(n);
+            x = 1/x;
         }
         
-        return healper(x,n);
+        if(n%2 == 0)
+            return myPow(x*x,n/2);
+        else
+            return x*myPow(x,n-1);
     }
 };
