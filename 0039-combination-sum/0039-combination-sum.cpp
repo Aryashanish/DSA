ac1535708e966a6d66d42755a721dd70ad4b5268
Arya_Shanish <70702833+Aryashanish@users.noreply.github.com>
2023-04-30 15:59:05 +0530

    Time: 66 ms (17.85%), Space: 41 MB (18.94%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    void healper(vector<vector<int>>&ans , vector<int> &A , int sum , int i, vector<int> &v){
-        if(sum == 0)
-            ans.push_back(v);
-        
-        if(i == A.size())
+    void healper(vector<vector<int>>& ans , vector<int> t , int sum , int i , vector<int>& v){
+        if(sum == 0){
+            ans.push_back(t);
+            return;
+        }
+        if(i == v.size())
             return;
         
-        while(i < A.size() && sum-A[i] >= 0){
-            v.push_back(A[i]);
-            healper(ans , A , sum-A[i] , i , v);
-            i++;
-            v.pop_back();
+        healper(ans , t , sum , i+1 , v);
+        if(sum - v[i] >= 0){
+            t.push_back(v[i]);
+            healper(ans , t , sum-v[i] , i , v);
         }
     }
-    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        sort(candidates.begin() , candidates.end());
-        
         vector<vector<int>> ans;
         vector<int> temp;
-        healper(ans , candidates , target , 0 , temp);
         
+        healper(ans , temp , target , 0 , candidates);
         return ans;
     }
 };
