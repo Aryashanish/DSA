508b076c4534f87de1a7ae91f030fd5bc0162f3c
Arya_Shanish <70702833+Aryashanish@users.noreply.github.com>
2023-08-10 00:58:48 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -9,29 +9,27 @@ using namespace std;
 class Solution
 {
     public:
-    int healper(string s1 , string s2 , int i , int j, vector<vector<int>>& dp){
-        if(i<0 || j<0)
+    //Function to find the length of longest common subsequence in two strings.
+    int healper(int i , int j , string &s1 , string &s2 , vector<vector<int>> &dp){
+        if(i < 0 || j < 0)
             return 0;
         
         if(dp[i][j] != -1)
             return dp[i][j];
         
         if(s1[i] == s2[j])
-            return dp[i][j] = (1 + healper(s1,s2,i-1,j-1,dp));
+            return dp[i][j] = 1 + healper(i-1,j-1,s1,s2,dp);
         
-        int temp1 = healper(s1,s2,i-1,j,dp);
-        int temp2 = healper(s1,s2,i,j-1,dp);
+        int t1 = healper(i-1,j,s1,s2,dp);
+        int t2 = healper(i,j-1,s1,s2,dp);
         
-        return dp[i][j] = max(temp1,temp2);
+        return dp[i][j] = max(t1,t2);
     }
-    
-    //Function to find the length of longest common subsequence in two strings.
-    int lcs(int x, int y, string s1, string s2)
+    int lcs(int n, int m, string s1, string s2)
     {
         // your code here
-        vector<vector<int>> dp(x+1 , vector<int>(y+1,-1));
-
-        return healper(s1,s2,x-1,y-1,dp);
+        vector<vector<int>> dp(n+1 , vector<int>(m+1,-1));
+        return healper(n-1,m-1,s1,s2,dp);
     }
 };
 
@@ -39,15 +37,15 @@ class Solution
 //{ Driver Code Starts.
 int main()
 {
-    int t,n,k,x,y;
+    int t,n,m;
     cin>>t;
     while(t--)
     {
-        cin>>x>>y;          // Take size of both the strings as input
+        cin>>n>>m;          // Take size of both the strings as input
         string s1,s2;
         cin>>s1>>s2;        // Take both the string as input
         Solution ob;
-        cout << ob.lcs(x, y, s1, s2) << endl;
+        cout << ob.lcs(n, m, s1, s2) << endl;
     }
     return 0;
 }
